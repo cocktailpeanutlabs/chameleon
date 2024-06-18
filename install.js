b3e3b4e250e295232ab47f65881a44c5692eb449
cocktailpeanut <cocktailpeanuts@proton.me>
2024-06-18 16:20:57 -0400

    update

--- install.js
+++ install.js
@@ -24,18 +24,18 @@ module.exports = {
         ]
       }
     },
-    // Delete this step if your project does not use torch
-    {
-      method: "script.start",
-      params: {
-        uri: "torch.js",
-        params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
-        }
-      }
-    },
+//    // Delete this step if your project does not use torch
+//    {
+//      method: "script.start",
+//      params: {
+//        uri: "torch.js",
+//        params: {
+//          venv: "env",                // Edit this to customize the venv folder path
+//          path: "app",                // Edit this to customize the path to start the shell from
+//          // xformers: true   // uncomment this line if your project requires xformers
+//        }
+//      }
+//    },
     // Edit this step with your custom install commands
     {
       method: "shell.run",
