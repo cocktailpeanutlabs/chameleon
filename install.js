20bd56f8c1cb985d547544b5edf7b06c96b5e281
cocktailpeanut <cocktailpeanuts@proton.me>
2024-06-18 16:34:17 -0400

    update

--- install.js
+++ install.js
@@ -24,18 +24,17 @@ module.exports = {
         ]
       }
     },
-//    // Delete this step if your project does not use torch
-//    {
-//      method: "script.start",
-//      params: {
-//        uri: "torch.js",
-//        params: {
-//          venv: "env",                // Edit this to customize the venv folder path
-//          path: "app",                // Edit this to customize the path to start the shell from
-//          // xformers: true   // uncomment this line if your project requires xformers
-//        }
-//      }
-//    },
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          xformers: true   // uncomment this line if your project requires xformers
+        }
+      }
+    },
     // Edit this step with your custom install commands
     {
       method: "shell.run",
@@ -44,6 +43,16 @@ module.exports = {
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
           "pip install -e ."
+        ]
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
+        venv: "../../../env",                // Edit this to customize the venv folder path
+        path: "app/chameleon/viewer/backend",                // Edit this to customize the path to start the shell from
+        message: [
+          "pip install -r requirements.txt"
         ]
       }
     },
